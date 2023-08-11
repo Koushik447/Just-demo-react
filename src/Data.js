5f3c9777a04d953d03f72e9897086ac3a057d330
Koushik447 <sarkarkoushik447@gmail.com>
2023-08-11 20:58:06 +0530

    Third commit

--- src/Data.js
+++ src/Data.js
@@ -19,14 +19,6 @@ export const filterData = [
     id: "5",
     title: "Lifestyle",
   },
-  {
-    id: "6",
-    title: "Blog",
-  },
-  {
-    id: "7",
-    title: "Video",
-  },
 ];
 
 export const apiUrl = "https://codehelp-apis.vercel.app/api/get-top-courses";
